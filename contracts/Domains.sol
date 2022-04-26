d0c2ff2c425dc3c84aa75d548717d0a7075837f1
yoea13 <yoea13@gmail.com>
2022-04-26 14:41:53 -0300

    avances menores

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -8,4 +8,19 @@ contract Domains {
   constructor() {
     console.log("Probando algo copado para escribir en consola");
   }
+
+	 // A register function that adds their names to our mapping
+	 // msg.sender es la wallet del que llama al contrato
+	 // calldata - indica donde el argumento name será almacenado
+	 // calldata es non-persistent y no puede ser modificado (se usa porque consume poco gas)  
+	 function register(string calldata name) public {
+      domains[name] = msg.sender;
+      console.log("%s has registered a domain!", msg.sender);
+  }
+
+  // This will give us the domain owners' address
+	// view luego de public indica que la funcion solo lee datos y no modificara nada
+  function getAddress(string calldata name) public view returns (address) {
+      return domains[name];
+  }
 }
