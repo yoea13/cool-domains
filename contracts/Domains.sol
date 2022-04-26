233a5b3fe2674dd617ff9fb9a8fdbd7110492eb9
yoea13 <yoea13@gmail.com>
2022-04-26 16:37:37 -0300

    avance

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,22 +5,38 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+  // A "mapping" data type to store their names
+  mapping(string => address) public domains;
+
+	// Checkout our new mapping! This will store values
+  mapping(string => string) public records;
+
   constructor() {
-    console.log("Probando algo copado para escribir en consola");
+      console.log("Algo copado para probar.");
   }
 
-	 // A register function that adds their names to our mapping
-	 // msg.sender es la wallet del que llama al contrato
-	 // calldata - indica donde el argumento name será almacenado
-	 // calldata es non-persistent y no puede ser modificado (se usa porque consume poco gas)  
-	 function register(string calldata name) public {
+  // A register function that adds their names to our mapping
+  function register(string calldata name) public {
+		// Check that the name is unregistered (explained in notes)
+		  require(domains[name] == address(0)); //like the void
       domains[name] = msg.sender;
       console.log("%s has registered a domain!", msg.sender);
   }
 
   // This will give us the domain owners' address
-	// view luego de public indica que la funcion solo lee datos y no modificara nada
   function getAddress(string calldata name) public view returns (address) {
+		// Check that the owner is the transaction sender
       return domains[name];
   }
+
+	function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+	function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
+  }
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,24 @@
 const main = async () => {
-  const domainContractFactory = await hre.ethers.getContractFactory('Domains'); //compila
-  const domainContract = await domainContractFactory.deploy(); 
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
-};
+  console.log("Contract deployed by:", owner.address);
+  
+  let txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("doom");
+  console.log("Propietario de dominio DOOM", domainAddress);
+
+  //prueba de escribir un no propietario
+  // txn = await domainContract.connect(randomPerson).setRecord("doom", "Ahora es mi dominio!");
+  // await txn.wait();
+
+  // const domainOwner = await domainContract.getAddress("doom");
+  // console.log("Owner of domain:", domainOwner);
+}
 
 const runMain = async () => {
   try {
